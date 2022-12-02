a4881dbdb6fcbfab521359e318c852db36cf68eb
NicolasKemp <kempnj@gmail.com>
2022-12-02 15:15:34 +0200

    8. Back to only home page

--- threed_viewer/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ThreedViewerConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'threed_viewer'
--- threegis/urls.py
+++ threegis/urls.py
@@ -16,11 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from landingpage.views import LandingPage
-from threed_viewer.views import ThreeViewer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', LandingPage.as_view()),
-    path('threeviewer', ThreeViewer.as_view())
+    path('', LandingPage.as_view())
 
 ]
